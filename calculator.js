06a5081169cf67e441fd6195760f8afe18974bbf
gjoao98 <gjoao98@gmail.com>
2021-07-29 19:48:50 -0300

    Calculadora funcionando em JS puro

---
+++ calculator.js
@@ -0,0 +1,18 @@
+function insert(num){
+        const number = document.getElementById('result').innerHTML;
+        document.getElementById('result').innerHTML = number + num;
+    }
+
+function rase() {
+    document.getElementById('result').innerHTML = "";
+}
+
+function calculate() {
+    const answer = document.getElementById('result').innerHTML;
+    if(result){
+        document.getElementById('result').innerHTML = eval(answer)
+    }
+    else {
+        document.getElementById('result').innerHTML = "Nada..."
+    }
+}
